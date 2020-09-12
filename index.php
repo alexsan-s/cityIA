beca79c08c41af8b98f4d58318d840690d9992f0
Alexsander da Silva <alexsans@outlook.com.br>
2020-09-12 08:45:13 -0300

    insertLast

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
-    include 'note.php';
+include 'node.php';
+$nodes = ["PMJN", "MML", "DT", "SDST", "CSFC", "MIST", "MHNT", "CR", "PM", "PVI", "MM"];
+$graphs = [
+    ["MML", "DT"],
+    ["PMJN", "DT", "CSFC"],
+    ["PMJN", "MML", "CSFC", "SDST"],
+    ["DT", "CSFC", "CR"],
+    ["MML", "SDST", "MIST", "MHNT", "CR"],
+    ["MHNT", "CSFC"],
+    ["MIST", "PM"],
+    ["SDST", "CSFC", "PVI"],
+    ["MHNT", "PVI"],
+    ["CR", "PM", "MM"],
+    ["PVI"],
+];
+$sol = new Search();
+$way = [];
+$source = "aa";
+$detiny = "aas";
 
-    class Index{
-        var $nodes = ["PMJN", "MML", "DT", "SDST", "CSFC", "MIST", "MHNT", "CR", "PM", "PVI", "MM"];
-        var $graphs = [
-            ["MML", "DT"], 
-            ["PMJN", "DT", "CSFC"], 
-            ["PMJN", "MML", "CSFC", "SDST"],
-            ["DT", "CSFC", "CR"],
-            ["MML", "SDST", "MIST", "MHNT", "CR"],
-            ["MHNT", "CSFC"],
-            ["MIST", "PM"],
-            ["SDST", "CSFC", "PVI"],
-            ["MHNT", "PVI"],
-            ["CR", "PM", "MM"],
-            ["PVI"],
-        ];
-        var $sol = new Node();
-        var $way = [];
-        var $source = "";
-        var $detiny = "";
-    }
+$sol->amplitude($origem, $destino);
+
+?>
--- node.php
+++ node.php
@@ -1,7 +1,9 @@
 <?php
-class Node{
+class Node
+{
     // var $father;
-    function __constructor($father, $value1, $value2, $previous, $next){
+    function __constructor($father = null, $value1 = null, $value2 = null, $previous = null, $next = null)
+    {
         $this->father   = $father;
         $this->value1   = $value1;
         $this->value2   = $value2;
@@ -10,7 +12,35 @@ class Node{
     }
 }
 
-class Lista{
-    var $head;
-    var $tail;
+class Lista
+{
+    var $head = null;
+    var $tail = null;
+
+    function insertLast($v1, $v2, $f){
+        $new_node = new Node($f, $v1, $v2, null, null);
+
+        if (is_null($this->head))
+            $this->head = $new_node;
+        else{
+            $this->tail->next = $new_node;
+            $new_node->previous = $this ->tail;
+        }          
+        $this->tail = $new_node;
+    }
+
+    function first(){
+        return $this->head;
+    }
+
+    function last(){
+        return $this->tail;
+    }
+}
+
+class Search{
+    function amplitude($origem, $destino){
+        $l1 = new lista();
+        $l2 = new lista();
+    }
 }
