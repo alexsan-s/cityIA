6c4633611cf1593b6b0ed6c0c437fa8abc1805ec
Alexsander da Silva <alexsans@outlook.com.br>
2020-10-06 14:44:26 -0300

    Try node

--- action.php
+++ action.php
@@ -1,5 +1,8 @@
 <?php
 include 'node.php';
+$jsonString = file_get_contents('data.json');
+$data = json_decode($jsonString, true);
+
 // $node = ["PMJN", "MML", "DT", "SDST", "CSFC", "MIST", "MHNT", "CR", "PM", "PVI", "MM"];
 // $graph = [
 //     ["DT", "MML"],
@@ -14,28 +17,10 @@ include 'node.php';
 //     ["CR", "MM", "PM"],
 //     ["PVI"],
 // ];
-$node = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "Z"];
+$node = ["A", "B", "C", "D", "E", "F", "G", "H", "P", "Q", "R", "S"];
 $graph = [
-    ["Z", "T", "S"], //A
-    ["U", "P", "G", "F"], //B
-    ["R", "P", "D"], //C
-    ["M", "C"], //D
-    ["H"], //E
-    ["S", "B"], //F
-    ["B"], //G
-    ["U", "E"], //H
-    ["V", "N"], //L
-    ["T", "M"], //I
-    ["L", "D"], //M
-    ["I"], //N
-    ["Z", "S"], //O
-    ["R", "C", "B"], //P
-    ["S", "P", "C"], //R
-    ["R", "O", "F", "A"], //S
-    ["L", "A"], //T
-    ["V", "H", "B"], //U
-    ["U", "I"], //V
-    ["O", "A"] //Z
+    [], ["A"], ["A"], ["E", "C", "B"], ["R", "H"], ["G", "C"], [],
+    ["Q", "P"], ["Q"], [], ["F"], ["P", "E", "D"]
 ];
 
 $sol = new Search();
@@ -45,47 +30,61 @@ $destiny = $_POST['destiny'];
 
 if (isset($_POST["amplitude"])) {
     $way = $sol->amplitude($source, $destiny, $node, $graph);
-    if (is_array($way)) {
-        foreach (array_reverse($way) as $key) {
-            echo "$key  ";
-        }
-    } else {
-        echo $way;
-    }
 } else if (isset($_POST["depth"])) {
     $way = $sol->depth($source, $destiny, $node, $graph);
-    if (is_array($way)) {
-        foreach (array_reverse($way) as $key) {
-            echo "$key  ";
-        }
-    } else {
-        echo $way;
-    }
 } else if (isset($_POST["depth_limit"])) {
     $way = $sol->depth_limit($source, $destiny, 4, $node, $graph);
-    if (is_array($way)) {
-        foreach (array_reverse($way) as $key) {
-            echo "$key  ";
-        }
-    } else {
-        echo $way;
-    }
 } else if (isset($_POST["iterative_deepening"])) {
     $way = $sol->iterative_deepening($source, $destiny, 4, $node, $graph);
-    if (is_array($way)) {
-        foreach (array_reverse($way) as $key) {
-            echo "$key  ";
-        }
-    } else {
-        echo $way;
-    }
 } else if (isset($_POST["bidirectional"])) {
     $way = $sol->bidirectional($source, $destiny, $node, $graph);
-    if (is_array($way)) {
-        foreach ($way as $key) {
-            echo "$key  ";
+}
+if (is_array($way)) {
+    foreach (array_reverse($way) as $key) {
+         echo "$key  ";
+    }
+} else {
+     echo $way;
+}
+$ids = array();
+if (is_array($way)) {
+    foreach ($data as $key => $entry) {
+        if ($key == "nodes") {
+            for ($i = 0; $i < sizeof($entry); $i++) {
+                for ($j = 0; $j < sizeof($way); $j++) {
+                    if ($data[$key][$i]['label'] == $way[$j]) {
+                        array_push($ids, $data[$key][$i]['id']);
+                        $data[$key][$i]['color'] = "#00f";
+                        break;
+                    } else {
+                        $data[$key][$i]['color'] = "#eee";
+                    }
+                }
+            }
         }
-    } else {
-        echo $way;
+        print_r($ids);
+        // echo "<p>";
+        if ($key == "edges") {
+            for ($i = 0; $i < sizeof($entry); $i++) {
+                for ($j = sizeof($ids) - 1; $j >= 0; $j--) {
+                    $temp = $j;
+                    $temp--;
+                    if ($data[$key][$i]['source'] == $ids[$j] && $data[$key][$i]['target'] == $ids[$temp]) {
+                        $data[$key][$i]['color'] = "#00f";
+                        break;
+                    } else {
+                        $data[$key][$i]['color'] = "#eee";
+                    }
+                    // echo $data[$key][$i]['source'];
+                }
+            }
+        }
     }
 }
+
+// echo "Corrigido<p>";
+
+// $newJsonString = json_encode($data);
+// file_put_contents('data.json', $newJsonString);
+// header("Location: index.php");
+// die();
--- index.php
+++ index.php
@@ -45,24 +45,16 @@
                     <option value="F">F</option>
                     <option value="G">G</option>
                     <option value="H">H</option>
-                    <option value="I">I</option>
-                    <option value="L">L</option>
-                    <option value="M">M</option>
-                    <option value="N">N</option>
-                    <option value="O">O</option>
                     <option value="P">P</option>
+                    <option value="Q">Q</option>
                     <option value="R">R</option>
                     <option value="S">S</option>
-                    <option value="T">T</option>
-                    <option value="U">U</option>
-                    <option value="V">V</option>
-                    <option value="Z">Z</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="exampleFormControlSelect1">Destino</label>
                 <select class="form-control" id="exampleFormControlSelect1" name="destiny">
-                    <option value="A">A</option>
+                <option value="A">A</option>
                     <option value="B">B</option>
                     <option value="C">C</option>
                     <option value="D">D</option>
@@ -70,18 +62,10 @@
                     <option value="F">F</option>
                     <option value="G">G</option>
                     <option value="H">H</option>
-                    <option value="I">I</option>
-                    <option value="L">L</option>
-                    <option value="M">M</option>
-                    <option value="N">N</option>
-                    <option value="O">O</option>
                     <option value="P">P</option>
+                    <option value="Q">Q</option>
                     <option value="R">R</option>
                     <option value="S">S</option>
-                    <option value="T">T</option>
-                    <option value="U">U</option>
-                    <option value="V">V</option>
-                    <option value="Z">Z</option>
                 </select>
             </div>
             <input class="btn btn-primary" type="submit" value="Amplitude" name="amplitude">
@@ -89,6 +73,7 @@
             <input class="btn btn-primary" type="submit" value="Profundidade Limitada" name="depth_limit">
             <input class="btn btn-primary" type="submit" value="Aprofundamento Interativo" name="iterative_deepening">
             <input class="btn btn-primary" type="submit" value="Bidirecional" name="bidirectional">
+            <a href="./teste.php"> Teste</a>
         </form>
         <div id="sigma_div" style="width: 700px; height: 300px;"></div>
     </div>
